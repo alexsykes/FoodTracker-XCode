b60ceb6fc73f82d4d3821a5a14f907d743a50537
Alex Sykes <alex@alexsykes.net>
2020-02-19 08:09:16 +0000

    Unit Tests added

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,36 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Alex on 19/02/2020.
+//  Copyright © 2020 Alex Sykes. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialisation
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
